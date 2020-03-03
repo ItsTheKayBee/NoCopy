fecb16b90a81212d34ef99b827d00b7c2db2b8c1
ItsTheKayBee <32770100+ItsTheKayBee@users.noreply.github.com>
2020-03-03 23:50:08 +0530

    DB integration done

--- hasher.py
+++ hasher.py
@@ -1,5 +1,4 @@
 import hashlib
-import PyPDF2
 import docx2txt
 from PIL import Image
 import imagehash
@@ -7,8 +6,8 @@ import distance
 from mp3hash import mp3hash
 
 
-def txt_hash():
-    file = "myfile.txt"
+def txt_hash(upload):
+    file = upload
     BLOCK_SIZE = 65536
 
     file_hash = hashlib.sha3_256()
@@ -21,33 +20,20 @@ def txt_hash():
     return file_hash.hexdigest()
 
 
-def pdf_hash():
-    pdfFileObject = open('myfile.pdf', 'rb')
-    pdfReader = PyPDF2.PdfFileReader(pdfFileObject)
-    count = pdfReader.numPages
-    fb = ''
-    for i in range(count):
-        page = pdfReader.getPage(i)
-        fb += page.extractText().strip()
-
-    return hashlib.sha3_256(fb.encode('utf-8')).hexdigest()
-
-
-def word_hash():
-    result = docx2txt.process("myfile.docx").strip()
+def word_hash(upload):
+    result = docx2txt.process(upload).strip()
     return hashlib.sha3_256(result.encode('utf-8')).hexdigest()
 
 
-def image_hash():
-    file = imagehash.phash(Image.open("myfile.jpg"))
+def image_hash(upload):
+    file = imagehash.phash(Image.open(upload))
     return str(file)
 
 
-def audio_hash():
-    file = "myfile.mp3"
+def audio_hash(upload):
+    file = upload
     return mp3hash(file, None, hashlib.sha3_256())
 
 
 def image_dist(a, b):
     return distance.hamming(a, b)
-
--- upload.py
+++ upload.py
@@ -1,9 +1,52 @@
+import datetime
 from flask import *
 import mysql.connector
 import time
 import hasher
+import os
 
+
+UPLOAD_FOLDER = 'static/'
+ALLOWED_EXTENSIONS = {'txt', 'png', 'jpg', 'jpeg', 'mp3', 'doc', 'docx'}
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+def update_database(f):
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        passwd="",
+        database="nocopy"
+    )
+    file_name=f.filename
+    ext = f.filename.split(".")[-1]
+    mycursor = mydb.cursor()
+    timestamp = time.time()
+    ts = datetime.datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')
+    sql = ''
+    val = ''
+    path = "static/" + file_name
+    if ext == 'txt':
+        sql = "INSERT INTO docs (path, time, hashval, type) VALUES (%s, %s,%s,%s)"
+        val = (path, ts, hasher.txt_hash(path), ext)
+    elif ext == 'docx' or ext == 'doc':
+        sql = "INSERT INTO docs (path, time, hashval, type) VALUES (%s, %s,%s,%s)"
+        val = (path, ts, hasher.word_hash(path), ext)
+    elif ext == 'mp3':
+        sql = "INSERT INTO music (path, time, hashval) VALUES (%s, %s,%s)"
+        val = (path, ts, hasher.audio_hash(path))
+    elif ext == 'jpg' or ext == 'png':
+        sql = "INSERT INTO pics (path, time, hashval) VALUES (%s, %s,%s)"
+        val = (path, ts, hasher.image_hash(path))
+    mycursor.execute(sql, val)
+    mydb.commit()
+    mycursor.close()
 
 
 @app.route('/')
@@ -14,41 +57,19 @@ def index():
 @app.route('/success', methods=['POST'])
 def success():
     if request.method == 'POST':
-        mydb = mysql.connector.connect(
-            host="localhost",
-            user="root",
-            passwd="",
-            database="nocopy"
-        )
-        f = request.files['uploaded-file']
-        file_name=f.filename
-        ext=f.filename.split(".")[-1]
-        mycursor = mydb.cursor()
-        ts=time.time()
-        sql=''
-        val=''
-        path="files/"+file_name
-        if ext == 'txt':
-            sql = "INSERT INTO docs (path, time, hashval, type) VALUES (%s, %s,%s,%s)"
-            val = (path,ts, hasher.txt_hash(), ext)
-        elif ext == 'docx' or ext == 'doc':
-            sql = "INSERT INTO docs (path, time, hashval, type) VALUES (%s, %s,%s,%s)"
-            val = (path,ts, hasher.word_hash(), ext)
-        elif ext == 'pdf':
-            sql = "INSERT INTO docs (path, time, hashval, type) VALUES (%s, %s,%s,%s)"
-            val = (path,ts,hasher.pdf_hash(),ext)
-        elif ext == 'mp3':
-            sql = "INSERT INTO music (path, time, hashval) VALUES (%s, %s,%s)"
-            val = (path,ts,hasher.audio_hash())
-        elif ext == 'jpg' or ext == 'png':
-            sql = "INSERT INTO pics (path, time, hashval) VALUES (%s, %s,%s)"
-            val = (path,ts,hasher.image_hash())
+        file = request.files['uploaded-file']
 
-        mycursor.execute(sql, val)
-
-        mydb.commit()
-        f.save(path)
-        return render_template("success.html", name=f.filename)
+        if file.filename == '':
+            return render_template('upload.html')
+        if file and allowed_file(file.filename):
+            filename = file.filename
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            update_database(file)
+            return render_template('success.html', filename=filename)
+        else:
+            return redirect('/failure')
+    else:
+        return redirect('/upload')
 
 
 @app.route('/upload')
@@ -56,30 +77,64 @@ def upload():
     return render_template("upload.html")
 
 
+@app.route('/failure')
+def failure():
+    return render_template("failure.html")
+
+
 @app.route('/pics')
 def pics():
-    return render_template("pics.html")
+    db = mysql.connector.connect(user='root', password='',
+                                 host='localhost',
+                                 database='nocopy')
+    cursor = db.cursor()
+    sql = 'select path from pics'
+    cursor.execute(sql)
+    data = cursor.fetchall()
+    db.close()
+    return render_template("pics.html", data=data)
 
 
 @app.route('/music')
 def music():
-    return render_template("music.html")
+    db = mysql.connector.connect(user='root', password='',
+                                 host='localhost',
+                                 database='nocopy')
+    cursor = db.cursor()
+    sql = "select path from music"
+    cursor.execute(sql)
+    data = cursor.fetchall()
+    db.close()
+    return render_template("music.html", data=data)
 
 
 @app.route('/text')
 def text():
-    return render_template("text.html")
-
-
-@app.route('/pdf')
-def pdf():
-    return render_template("pdf.html")
+    db = mysql.connector.connect(user='root', password='',
+                                 host='localhost',
+                                 database='nocopy')
+    cursor = db.cursor()
+    sql = "select path from docs where type='txt'"
+    cursor.execute(sql)
+    data = cursor.fetchall()
+    db.close()
+    return render_template("text.html", data=data)
 
 
 @app.route('/word')
 def word():
-    return render_template("word.html")
+    db = mysql.connector.connect(user='root', password='',
+                                 host='localhost',
+                                 database='nocopy')
+    cursor = db.cursor()
+    sql = "select path from docs where type='doc' or type='docx'"
+    cursor.execute(sql)
+    data = cursor.fetchall()
+    db.close()
+    return render_template("word.html", data=data)
 
 
 if __name__ == '__main__':
+    app.secret_key = 'nocopykey'
+    app.config['SESSION_TYPE'] = 'filesystem'
     app.run(debug=True)
